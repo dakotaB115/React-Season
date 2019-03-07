bcc32a8055c170d3c87fddfd3aeb2ab1894e7051
Dakota Brown <jubrowning@MacBook-Pro-25.local>
2019-03-07 14:19:48 -0600

    finished

--- src/LoadingSpinner.js
+++ src/LoadingSpinner.js
@@ -3,9 +3,13 @@ import React from 'react';
 const Spinner = (props) => {
   return (
       <div className="ui active dimmer">
-        <div className="ui big text loader">{props.showMessage}</div>
+        <div className="ui big text loader">{props.Message}</div>
       </div>
   );
+};
+
+Spinner.defaultProps = {
+  Message: 'Loading...'
 };
 
 export default Spinner;
--- src/index.js
+++ src/index.js
@@ -13,16 +13,24 @@ class App extends React.Component {
     );
   };
 
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />
+    }
+
+    return <Spinner Message="Please accept location request" />;
+  };
+
   render() {
-      if (this.state.errorMessage && !this.state.lat) {
-        return <div>Error: {this.state.errorMessage}</div>;
-      }
-
-      if (!this.state.errorMessage && this.state.lat) {
-        return <SeasonDisplay lat={this.state.lat} />
-      }
-
-      return <Spinner showMessage="Please accept location request" />;
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    )
   };
 };
 
